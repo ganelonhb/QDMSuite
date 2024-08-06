e48c68b1a18df1940069646bb8cd29d64ccb67ec
ganelonhb <justyouraveragejoe@outlook.com>
2024-08-05 21:02:58 -0700

    js tool update

--- tools/create_inspection_file.py
+++ tools/create_inspection_file.py
@@ -1,11 +1,13 @@
 import argparse
 import jsbeautifier
 import os
+import subprocess
 import sys
 
 def main() -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument("dir", type=str, help="The directory to find .js files in")
+    parser.add_argument("--test", action="store_true", help="test the js files")
     args = parser.parse_args()
 
     with open("out.js", 'w') as log:
@@ -14,7 +16,18 @@ def main() -> int:
                 if file_name.endswith(".js"):
                     with open(os.path.join(root, file_name), 'r') as f:
                         log.write(f"/* START {file_name}\n------------------------------------------------*/\n\n")
-                        log.write(jsbeautifier.beautify(f.read()))
+                        script = jsbeautifier.beautify(f.read())
+                        log.write(script)
+                        if args.test:
+                            process = subprocess.Popen(['node', '-'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+                            stdout, stderr = process.communicate(input=f"{script}console.log(nameGen())")
+
+                            print(f"{os.path.join(root, file_name)} : {stdout}")
+                            with open("logtest.out", 'a') as f:
+                                f.write(f"{os.path.join(root, file_name)} : {stdout}\n\n")
+
+
                         log.write(f"/* END {file_name}\n------------------------------------------------*/\n\n")
 
 
